2a0e3594528e169b9e7434e88fb5ed42f978912d
DayannaSilva <sdayanna56@gmail.com>
2018-11-12 02:34:48 -0500

    Write, Read, Modify, Delete (File)

--- Labs/Unity 2/Lab10-Archivos/ControlSentencesAndOperators_Lab10/src/controlsentencesandoperators_lab10/Lab10_ControlSentencesAndOperators.java
+++ Labs/Unity 2/Lab10-Archivos/ControlSentencesAndOperators_Lab10/src/controlsentencesandoperators_lab10/Lab10_ControlSentencesAndOperators.java
@@ -7,9 +7,12 @@ package controlsentencesandoperators_lab10;
 
 import ec.edu.espe.ControlSentencesAndOperators_Lab10.model.Operation;
 import ec.edu.espe.ControlSentencesAndOperators_Lab10.model.Person;
+
 import ec.edu.espe.Files_Lab10.model.Files;
 import java.io.File;
+import static java.lang.System.in;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 /**
@@ -17,50 +20,128 @@ import java.util.Scanner;
  * @author DayannaSilva
  */
 public class Lab10_ControlSentencesAndOperators {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws Exception {
-        Scanner in = new Scanner (System.in);
-        int day,month,year;
-        Files arch = new Files();
-        File file = new File ("Archivo.csv");//Crear el archivo       
-        String name, data;
+    static int day = 0, month = 0, year = 0;
+    static Scanner in = new Scanner(System.in);
+    static Calendar cal = new GregorianCalendar();
+    static Files arch = new Files();
+    static File file = new File("Age.csv");
+    static Person person = new Person(year, month, day);
+    static String name, data;
+    public static void main(String[] args) {
+        byte op;
+        do{
+        System.out.println("*** Registro de Edades ***");
         
-        int valid = 0;
-            
-        System.out.println("-------****EDAD****-------");
+        System.out.println("**************************");
+        System.out.println("*   Que desea hacer????   *");
+        System.out.println("**************************");
+        System.out.println("    1. Ingresar Datos");
+        System.out.println("    2. Leer Datos");
+        System.out.println("    3. Modificar Datos");
+        System.out.println("    4. Borrar Datos");
+        System.out.println("    5. Salir");
+        do{
+            System.out.println("* Elija una opcion *");
+            op = in.nextByte();
+        }while(op <= 0 || op > 6);
+        
+        switch(op){
+            case 1:
+               writer();
+                break;
+            case 2:
+               read(true);
+                break;
+            case 3:
+               modify();
+                break;
+            case 4:
+               delete();
+                break;
+            case 5:
+                System.out.println("Fin");
+                break;
+        }
+        }while(op != 5);
+    }
+    public static void read(boolean check){
+        System.out.print("*** Registro ***");
+        arch.readFile(file);
+        if(check){
+        System.out.println("* Digite cualquier cosa para regresar al Menu*");
+        in.next();
+        }
+    }
+    public static void modify(){
+        read(false);
+        String nameModify;
+        System.out.println("Que Nombre desea moodificar");
+        nameModify = in.nextLine();
+        String inString = Info();
+        arch.modifyFile(file, nameModify, data, true);
+        
+        System.out.println("** Registro Modificado **");
+        System.out.println("* Elija otra opcion *");
+        in.next();
+    }
+    public static void delete(){
+        read(false);
+        String otherName;
+        in.nextLine();
+        System.out.println("Escriba el nombre del que desea eliminar los datos");
+        otherName = in.nextLine();
+        
+        arch.deleteFile(file, otherName);
+    }
+    public static void writer(){        
+        data = Info();
+        
+        arch.write(file, data);
+        System.out.println("*** Registro Exitoso!!!!! ***\n"
+                + "Nombre: " + name + " " +
+                + person.getAgeYear() + " años "
+                        + person.getAgeMonth() + " meses "
+                                + person.getAgeDay() +" dias");
+        System.out.println("* Elija otra opcion *");
+        in.next();
+    }
+     public static String Info(){
+        boolean check = false;
+        in.nextLine();
         System.out.println("Ingrese su Nombre");
-        name = in.nextLine(); 
-        System.out.println("Ingrese el año de nacimiento");
-        year = in.nextInt();
-        if (year < 0) {
-            System.out.println("Año erroneo");
-            System.out.println("Ingrese el años positivo");
+        name = in.nextLine();
+        while (!check){
+            System.out.println("Ingrese el año de nacimiento");
             year = in.nextInt();
-        }
-        System.out.println("Ingrese el mes de nacimiento");
-        month = in.nextInt();
-        if (month < 0 | month > 12) {
-            System.out.println("Mes erroneo");
-            System.out.println("Ingrese meses del 1 al 12");
+            if (year < 0) {
+                System.out.println("Año erroneo");
+                System.out.println("Ingrese el años positivo");
+                year = in.nextInt();
+                check = true;
+            }
+            System.out.println("Ingrese el mes de nacimiento");
             month = in.nextInt();
-        }
-        System.out.println("Ingrese el dia de nacimiento");
-        day = in.nextInt();
-        if (day < 0 | day > 31) {
-            System.out.println("Dia erroneo");
-            System.out.println("Ingrese el dia positivos o menores que 31");
+            if (month < 0 | month > 12) {
+                System.out.println("Mes erroneo");
+                System.out.println("Ingrese meses del 1 al 12");
+                month = in.nextInt();
+                check = true;
+            }
+            System.out.println("Ingrese el dia de nacimiento");
             day = in.nextInt();
+            if (day < 0 | day > 31) {
+                System.out.println("Dia erroneo");
+                System.out.println("Ingrese el dia positivos o menores que 31");
+                day = in.nextInt();
+                check = true;
+            }
+            if(check) check = false;
+            else check = true;
         }
-        Person person = new Person(year, month, day);
-        person.calcAge(year,month,day); 
-
-        data = name + "," + person.getAgeYear() + "," + person.getAgeMonth() + "," + person.getAgeDay();
-        arch.Write(file, data);
-        System.out.println("***Edades***");
-        arch.Read(file);
-        }  
-        
+        person.calcAge(year,month,day);
+        return "("+ name + "), " + person.getAgeYear() + ", " 
+                + person.getAgeMonth() + ", " + person.getAgeDay();
+       
+     }
+    
 }
--- Labs/Unity 2/Lab10-Archivos/ControlSentencesAndOperators_Lab10/src/ec/edu/espe/Files_Lab10/model/Files.java
+++ Labs/Unity 2/Lab10-Archivos/ControlSentencesAndOperators_Lab10/src/ec/edu/espe/Files_Lab10/model/Files.java
@@ -11,6 +11,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 
@@ -19,7 +20,7 @@ import java.io.IOException;
  * @author DayannaSilva
  */
 public class Files{
-    public void Write(File file, String inString){
+    public void write(File file, String inString){
         try{
             BufferedWriter buffWriter = new BufferedWriter(
                     new FileWriter(file, true));
@@ -33,7 +34,7 @@ public class Files{
         }
     }  
     
-    public void Read(File file){
+    public void readFile(File file){
                 String line = "";
         try{
             BufferedReader reader = new BufferedReader(new FileReader(file));
@@ -46,6 +47,59 @@ public class Files{
             System.out.println("No se encontro el archivo");
         }
     }
+    public ArrayList<String> read(File file){
+        ArrayList<String> line = new ArrayList<String>();
+        
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            int i =0;
+            line.add("");
+            while(line.get(i) != null){
+                line.add(reader.readLine());
+                i++;
+            }
+            reader.close();
+        }catch(IOException e){
+            System.out.println("No se encontro el archivo");
+        }
+        return line;
+    }
+    
+    
+    public void modifyFile(File file, String name, String inString, boolean check){
+        ArrayList<String> line = new ArrayList<String>();
+        line = read(file);
+        
+        if(check){
+            for (int i = 0; i < line.size()-1; i++)
+                if(line.get(i).contains(name))
+                    line.set(i, inString);
+        }else{
+            for (int i = 0; i < line.size()-1; i++)
+                if(line.get(i).contains(name))
+                    line.remove(i);
+        }  
+        try{
+            
+            BufferedWriter buffWriter = new BufferedWriter(
+                    new FileWriter(file));
+            for(int i = 1; i < line.size()-1; i++){
+                buffWriter.write(line.get(i));
+                buffWriter.newLine();
+            }
+            buffWriter.close();
+            
+        }catch(IOException e){
+            System.out.println("No se encontro el archivo o"
+                    + " ha ocurrido un error fatal :)");
+        }
+    }
+    
+    public void deleteFile(File file, String name){
+        modifyFile(file, name, null, false);
+    }
+    
+    
                 
     
     
