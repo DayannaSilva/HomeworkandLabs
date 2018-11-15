fdd714e29c50a9a96a753aedd7ba72d52cd18a5e
DayannaSilva <sdayanna56@gmail.com>
2018-11-14 23:45:45 -0500

    Lab 11 Arrays

---
+++ Labs/Unity 2/Lab11_Arrays/Lab11_Arrays/src/ec/edu/espe/Lab11_Arrays/model/Operation.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.Lab11_Arrays.model;
+
+/**
+ *
+ * @author DayannaSilva
+ */
+public class Operation {
+    private int operand1;
+    private int operand2;
+    private int result;
+    public int add (int operand1, int operand2){
+        result = operand1 + operand2;
+        return result;
+    }
+    public int subs (int operand1, int operand2){
+        result = add(operand1,(-operand2));
+        return result;
+    }
+    public int multiplication (int operand1, int operand2){
+        int cont=0;
+        int acum=0;
+        boolean cheak = false;
+        //Valor absoluto
+        if (operand2<0){
+            operand2 = (-operand2);
+            cheak = true;
+        }
+        while (cont<operand2){
+            acum = add(acum,operand1);
+            cont++;
+        }
+        if (cheak) return -acum;
+        else return acum;
+    }
+    public int division (int operand1, int operand2){
+        int cont=0;
+        while(operand1>=operand2){
+            operand1=add(operand1,(-operand2));
+            cont++;
+        }
+        System.out.print("El residuo es:"+operand1);
+        return cont;
+    }
+
+    
+
+    
+}
---
+++ Labs/Unity 2/Lab11_Arrays/Lab11_Arrays/src/lab11_arrays/Lab11_Arrays.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab11_arrays;
+
+import ec.edu.espe.Lab11_Arrays.model.Files;
+import ec.edu.espe.Lab11_Arrays.model.Person;
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ *
+ * @author DayannaSilva
+ */
+public class Lab11_Arrays {
+    static File file; 
+    static Files arch;
+
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        int year,month,day;
+        String name,data;
+        
+        Person[] person = new Person[10]; 
+        Files filen = new Files();
+        File file = new File("archivo.csv");
+        
+        for(int i=0;i<person.length;i++){
+        
+            System.out.print("Ingrese el nombre: ");
+            name = in.nextLine();
+            
+            System.out.println("Ingrese el año de nacimiento");
+            year = in.nextInt();
+            if (year < 0) {
+                System.out.println("Año erroneo");
+                System.out.println("Ingrese el años positivo");
+                year = in.nextInt();
+                
+            }
+            System.out.println("Ingrese el mes de nacimiento");
+            month = in.nextInt();
+            if (month < 0 | month > 12) {
+                System.out.println("Mes erroneo");
+                System.out.println("Ingrese meses del 1 al 12");
+                month = in.nextInt();
+                
+            }
+            System.out.println("Ingrese el dia de nacimiento");
+            day = in.nextInt();
+            if (day < 0 | day > 31) {
+                System.out.println("Dia erroneo");
+                System.out.println("Ingrese el dia positivos o menores que 31");
+                day = in.nextInt();
+              
+            }
+            in.nextLine();
+            
+            person[i] = new Person(year,month,day);                   
+            person[i].calcAge(year,month,day);   
+            data = "Nombre:" + ":" + name + "edad:" + ":" + person[i].getAgeYear();
+            filen.write(file, data);
+            
+         }
+    }
+    
+}
